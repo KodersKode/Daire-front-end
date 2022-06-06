3151315c04e20bd184abe6cea168b9f5ffa38eee
AmandaRichards <Richards.amanda92@gmail.com>
2022-06-06 14:07:13 +0100

    testing navButton in Dashboard

--- src/component/pages/dashboard/index.tsx
+++ src/component/pages/dashboard/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import NavButton from "../../navButton";
 // import { TDashboardProps } from "./index.definition";
 
 function Dashboard() {
-  return <div>"Dashboard"</div>;
+  return <div>
+    <p>"Dashboard"</p>
+    <NavButton name='Neuva sesion'/></div>;
 }
 
 export default Dashboard;
