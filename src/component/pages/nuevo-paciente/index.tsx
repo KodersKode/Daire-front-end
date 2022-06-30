75d07d613781a6609daf29d6e9dc18c7185d6b98
Israel <93683580+blueishegg@users.noreply.github.com>
2022-06-30 11:22:44 +0100

    completing section 1 of form

--- src/component/pages/nuevo-paciente/index.tsx
+++ src/component/pages/nuevo-paciente/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import SmallTextBox from "../../smallTextBox";
 import { useState } from "react";
 import css from "./index.module.css";
-import DatePicker from "react-date-picker";
 import DropDown from "../../dropDown";
+import DateSelector from "../../dateSelector";
 
 function NuevoPaciente() {
   const [dob, setDob] = useState(new Date());
@@ -13,6 +13,8 @@ function NuevoPaciente() {
   const [curso, setCurso] = useState(""); //class
   const [tutor, setTutor] = useState(""); //tutor
   const [padres, setPadres] = useState(""); //parent
+  const [telefono, setTelefono] = useState(""); //phone-number
+  const [email, setEmail] = useState(""); //email
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setNombre(e.target.value);
@@ -34,56 +36,85 @@ function NuevoPaciente() {
     <form className={css.form}>
       <h1 className={css.header}>Nuevo Paciente</h1>
       <div className={css.wrapper}>
-      <DropDown />
-      <label htmlFor='Fecha'>fecha</label>
-      <DatePicker name="Fecha"/>
+        <DropDown />
+        <DateSelector label="Fecha" />
       </div>
       <div className={css.section}>
-        
         <h1>Datos personales</h1>
         <div className={css.subsection}>
-        <SmallTextBox label="Nombre" value={nombre} onChange={handleChange} className={css.nombre} />
-        <SmallTextBox
-          label="Apellido"
-          value={apellido}
-          onChange={(e) => {
-            setApellido(e.target.value);
-          }}
-          className={css.apellido}
-        />
-        <DatePicker onChange={setDob} value={dob} className={css.date}/>
-        <SmallTextBox value={getAge(dob)} label="Edad"className={css.apellido} />
+          <SmallTextBox
+            label="Nombre"
+            value={nombre}
+            onChange={handleChange}
+            className={css.nombre}
+          />
+          <SmallTextBox
+            label="Apellido"
+            value={apellido}
+            onChange={(e) => {
+              setApellido(e.target.value);
+            }}
+            className={css.apellido}
+          />
+          <DateSelector
+            label="Fecha de nacimiento"
+            onChange={setDob}
+            value={dob}
+            className={css.date}
+          />
+          <SmallTextBox
+            value={getAge(dob)}
+            label="Edad"
+            className={css.apellido}
+          />
         </div>
         <h1>Escolares</h1>
         <div className={css.subsection}>
-        <SmallTextBox
-          label="Escuela"
-          value={escuela}
-          onChange={(e) => {
-            setEscuela(e.target.value);
-          }}
-        />
-        <SmallTextBox
-          label="Curso"
-          value={curso}
-          onChange={(e) => {
-            setCurso(e.target.value);
-          }}
-        />
-        <SmallTextBox
-          label="Tutor/Tutora"
-          value={tutor}
-          onChange={(e) => {
-            setTutor(e.target.value);
-          }}
-        />
-        <SmallTextBox
-          label="Nombre de los padres"
-          value={padres}
-          onChange={(e) => {
-            setPadres(e.target.value);
-          }}
-        />
+          <SmallTextBox
+            label="Escuela"
+            value={escuela}
+            onChange={(e) => {
+              setEscuela(e.target.value);
+            }}
+          />
+          <SmallTextBox
+            label="Curso"
+            value={curso}
+            onChange={(e) => {
+              setCurso(e.target.value);
+            }}
+          />
+          <SmallTextBox
+            label="Tutor/Tutora"
+            value={tutor}
+            onChange={(e) => {
+              setTutor(e.target.value);
+            }}
+          />
+          <SmallTextBox
+            label="Nombre de los padres"
+            value={padres}
+            onChange={(e) => {
+              setPadres(e.target.value);
+            }}
+          />
+        </div>
+        <h1>Datos de contacto</h1>
+        <div className={css.subsection}>
+          <SmallTextBox
+            label="Telefono o de los padres si es menor"
+            value={telefono}
+            onChange={(e) => {
+              setTelefono(e.target.value);
+            }}
+          />
+          <SmallTextBox
+            label="Email de contacto"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
         </div>
       </div>
     </form>
